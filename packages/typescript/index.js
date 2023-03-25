9c68493409373d1d6e46e5a82f1dd187ab514378
rainbowatcher <rainbow-w@qq.com>
2023-03-25 15:47:34 +0800

    chore: update eslint-config-ts

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -1,8 +1,45 @@
 
-let a = 2
+const tsconfig = process.env.ESLINT_TSCONFIG || "tsconfig.eslint.json"
 
-export default {
+module.exports = {
+  extends: [
+    "@rainbowatcher/eslint-config-js",
+    "plugin:import/typescript",
+    "plugin:import/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+    "import/parsers": {
+      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx"],
+    },
+  },
+  overrides: [
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint",
+  ],
   rules: {
-    "prefer-const": ["warn", { destructuring: "any", ignoreReadBeforeAssign: true }]
-  }
+    "no-empty-function": "off",
+    "@typescript-eslint/no-empty-function": "warn",
+    // off
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/ban-types": "off",
+    "no-duplicate-imports": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+  },
 }
