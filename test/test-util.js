5ba091128b9a6fc1c5d0c8f31ca181598c20315c
rainbowatcher <rainbow-w@qq.com>
2023-05-25 21:44:12 +0800

    test: add logic when test typescript rules

--- test/test-util.js
+++ test/test-util.js
@@ -6,17 +6,22 @@ function hasRule(errors, ruleId) {
   return errors.some(error => error.ruleId === ruleId)
 }
 
-async function runEslint(string, package) {
-  const packageName = path.join(process.cwd(), `packages/${package}`)
-  const eslint = new ESLint({ useEslintrc: false, overrideConfig: require(packageName) })
-  const [firstResult] = await eslint.lintText(string)
+async function runEslint(string, eslintConfig, filePath) {
+  const eslint = new ESLint({ useEslintrc: false, overrideConfig: eslintConfig })
+  const [firstResult] = await eslint.lintText(string, { filePath })
   return firstResult.messages
 }
 
-module.exports = function (package) {
+module.exports = function (pkg) {
+  const packageName = path.join(process.cwd(), `packages/${pkg}`)
+  const eslintConfig = require(packageName)
+  let filePath = undefined
+  if (pkg === "typescript") {
+    filePath = "test/_.ts"
+  }
   return function (rule, input, expect = true) {
     test(`[${rule}] >>> ${input} - ${expect}`, async (t) => {
-      const errors = await runEslint(input, package)
+      const errors = await runEslint(input, eslintConfig, filePath)
       const has = hasRule(errors, rule)
       const msg = JSON.stringify(errors, null, 2)
       expect ? t.true(has, msg) : t.false(has, msg)
