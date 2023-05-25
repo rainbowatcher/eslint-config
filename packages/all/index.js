da55ed1f82a9e86ba650f810cf412855cc61981f
rainbowatcher <rainbow-w@qq.com>
2023-05-25 22:17:12 +0800

    fix(json,javascript): add missing dependency

--- packages/all/index.js
+++ packages/all/index.js
@@ -1,6 +1,8 @@
-module.exports = {
+const { defineConfig } = require("eslint-define-config")
+
+module.exports = defineConfig({
   extends: [
     "@rainbowatcher/eslint-config-vue",
     "@rainbowatcher/eslint-config-json",
   ],
-}
+})
