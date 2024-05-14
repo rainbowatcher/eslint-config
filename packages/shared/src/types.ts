515771e4810ac1357ad82418a86831823cc8010b
rainbowatcher <rainbow-w@qq.com>
2024-05-14 22:56:27 +0800

    chore: remove default values for type Option

--- packages/shared/src/types.ts
+++ packages/shared/src/types.ts
@@ -7,76 +7,67 @@ export type EslintFlatConfigItem = {
 
 export type EslintFlatConfigs = Array< Awaitable < Arrayable<EslintFlatConfigItem>>>
 
+/**
+ * there has no default value for each properties, all properties are optional and be set by defineConfig
+ */
 export type Options = {
     /**
      * enable eslint for css
-     * @default false
      */
     css?: boolean
 
     /**
      * enable git ignore intergration
-     * @default true
      */
     gitignore?: boolean
 
     /**
      * enable lint for graphql
-     * @default false
      */
     graphql?: boolean
 
     /**
      * enable lint for json
-     * @default true
      */
     json?: boolean
 
     /**
      * enable lint for jsx
-     * @default false
      */
     jsx?: boolean
 
     /**
      * enable lint for markdown
-     * @default true
      */
     markdown?: boolean
 
     /**
      * enable stylistic rules
-     * @default true
      */
     style?: boolean
 
     /**
      * enable lint for toml
-     * @default false
      */
     toml?: boolean
 
     /**
      * enable lint for typescript
-     * @default true
      */
     typescript?: boolean
 
     /**
      * enable lint for unocss
-     * @default false
      */
     unocss?: boolean
 
     /**
      * enable eslint for vue
-     * @default false
      */
     vue?: boolean
 
     /**
      * enable lint for yaml
-     * @default false
      */
     yaml?: boolean
 
