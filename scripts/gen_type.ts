3cf32d6925aebe2e16586fe39835895025d4c59a
rainbowatcher <rainbow-w@qq.com>
2024-05-14 21:08:38 +0800

    chore: add ignore module in gen_type.ts

--- scripts/gen_type.ts
+++ scripts/gen_type.ts
@@ -1,6 +1,7 @@
 import fs from "node:fs/promises"
 import { concat } from "eslint-flat-config-utils"
 import { flatConfigsToRulesDTS } from "eslint-typegen/core"
+import ignoreConfigs from "packages/ignore/src"
 import { jsConfigs } from "packages/javascript/src"
 import { jsonConfigs } from "packages/json/src"
 import { mdConfigs } from "packages/markdown/src"
@@ -8,6 +9,7 @@ import { prettierConfigs } from "packages/prettier/src"
 import { tsConfigs } from "packages/typescript/src"
 import { vueConfigs } from "packages/vue/src"
 import type { Linter } from "eslint"
+import type { Options } from "packages/shared/src"
 
 async function genType(
     configs: Linter.FlatConfig[],
@@ -19,10 +21,23 @@ async function genType(
     await fs.writeFile(dist, dts)
 }
 
-const opts = {
-    json: true, markdown: true, typescript: true, vue: true,
+const opts: Required<Options> = {
+    css: true,
+    gitignore: true,
+    graphql: true,
+    json: true,
+    jsx: true,
+    markdown: true,
+    style: true,
+    toml: true,
+    typescript: true,
+    unocss: true,
+    vue: true,
+    yaml: true,
 }
+
 const configs = {
+    ignore: ignoreConfigs(opts),
     javascript: jsConfigs(opts),
     json: jsonConfigs(opts),
     markdown: mdConfigs(opts),
