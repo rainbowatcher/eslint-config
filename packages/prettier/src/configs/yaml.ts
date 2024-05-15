2737a8a7bc0d624ab73f1b4d4642bd6d5225a096
rainbowatcher <rainbow-w@qq.com>
2024-05-15 22:57:02 +0800

    fix(prettier): add yaml eslint parser

--- packages/prettier/src/configs/yaml.ts
+++ packages/prettier/src/configs/yaml.ts
@@ -1,16 +1,24 @@
-import { GLOB_YAML } from "@rainbowatcher/eslint-config-shared"
+import { GLOB_YAML, interopDefault } from "@rainbowatcher/eslint-config-shared"
 import { prettierOptions } from "../options"
 import type { EslintFlatConfigItem, Options } from "@rainbowatcher/eslint-config-shared"
 
-export function yaml(opts: Options): EslintFlatConfigItem {
+export async function yaml(opts: Options): Promise<EslintFlatConfigItem> {
     if (!opts.style || !opts.yaml) return {}
+
+    const parserYaml = await interopDefault(import("yaml-eslint-parser"))
     return {
         files: [GLOB_YAML],
+        languageOptions: {
+            parser: parserYaml,
+        },
         name: "rainbowatcher:prettier:yaml",
         rules: {
             "prettier/prettier": ["error", {
                 ...prettierOptions,
                 parser: "yaml",
+
+                // vscode always treats tab as 2 spaces for yaml file
+                tabWidth: 2,
             }, {
                 usePrettierrc: false,
             }],
