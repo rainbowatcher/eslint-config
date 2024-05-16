52d4309ec90ffd73d6662c6fb7958caa89435855
rainbowatcher <rainbow-w@qq.com>
2024-05-16 22:02:44 +0800

    feat: add types in gen_type.ts

--- scripts/gen_type.ts
+++ scripts/gen_type.ts
@@ -2,20 +2,24 @@ import fs from "node:fs/promises"
 import { concat } from "eslint-flat-config-utils"
 import { flatConfigsToRulesDTS } from "eslint-typegen/core"
 import ignoreConfigs from "packages/ignore/src"
-import { jsConfigs } from "packages/javascript/src"
-import { jsonConfigs } from "packages/json/src"
-import { mdConfigs } from "packages/markdown/src"
-import { prettierConfigs } from "packages/prettier/src"
-import { tsConfigs } from "packages/typescript/src"
-import { vueConfigs } from "packages/vue/src"
+import jsConfigs from "packages/javascript/src"
+import jsonConfigs from "packages/json/src"
+import mdConfigs from "packages/markdown/src"
+import prettierConfigs from "packages/prettier/src"
+import tsConfigs from "packages/typescript/src"
+import unocssConfigs from "packages/unocss/src"
+import vueConfigs from "packages/vue/src"
+import p from "picocolors"
 import type { Linter } from "eslint"
-import type { Options } from "packages/shared/src"
+import type { Module } from "packages/all/src/types"
+import type { EslintFlatConfigs, Options } from "packages/shared/src"
 
 async function genType(
     configs: Linter.FlatConfig[],
     dist: string,
     extra?: string,
 ) {
+    console.log(`${p.green("generated")} ${dist}`)
     let dts = await flatConfigsToRulesDTS(configs)
     dts += extra ?? ""
     await fs.writeFile(dist, dts)
@@ -36,13 +40,14 @@ const opts: Required<Options> = {
     yaml: true,
 }
 
-const configs = {
+const configs: Record<Module, EslintFlatConfigs> = {
     ignore: ignoreConfigs(opts),
     javascript: jsConfigs(opts),
     json: jsonConfigs(opts),
     markdown: mdConfigs(opts),
     prettier: prettierConfigs(opts),
     typescript: tsConfigs(opts),
+    unocss: unocssConfigs(opts),
     vue: vueConfigs(opts),
 }
 
