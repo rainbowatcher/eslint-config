8b9a517c327eca476008612ee572acf094748a9a
rainbowatcher <rainbow-w@qq.com>
2024-05-21 22:31:25 +0800

    chore: add css/uno in eslint config and add new uno.config.js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "@rainbowatcher/eslint-config"
 
 export default defineConfig({
+    css: true,
     gitignore: true,
     json: true,
     markdown: true,
     style: true,
     typescript: true,
+    unocss: true,
     vue: true,
     yaml: true,
 })
