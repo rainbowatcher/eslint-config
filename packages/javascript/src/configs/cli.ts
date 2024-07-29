05211df96da7f0d4c9030f83dd4d778e5b0378cc
rainbowatcher <rainbow-w@qq.com>
2024-07-29 12:48:08 +0800

    feat(javascript): add file for cli config

--- packages/javascript/src/configs/cli.ts
+++ packages/javascript/src/configs/cli.ts
@@ -2,11 +2,12 @@ import type { EslintFlatConfigItem } from "@rainbowatcher/eslint-config-shared"
 
 export function cli(): EslintFlatConfigItem {
     return {
-        files: ["**/bin/*.?([cm])js"],
+        files: ["**/bin/*.?([cm])js", "**/cli.?([cm])[jt]s"],
         name: "rainbowatcher:js:cli",
         rules: {
             "antfu/no-import-dist": "off",
             "unicorn/filename-case": "off",
+            "unicorn/no-process-exit": "off",
             "unicorn/prefer-module": "off",
         },
     }
