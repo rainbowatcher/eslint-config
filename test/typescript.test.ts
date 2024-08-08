a0207baa8c50c29eef39b8d2b8788da9192b6220
rainbowatcher <rainbow-w@qq.com>
2024-08-08 16:54:55 +0800

    test(typescript): try fix typescript test fail

--- test/typescript.test.ts
+++ test/typescript.test.ts
@@ -45,42 +45,42 @@ describe.concurrent("rules", () => {
         }
     `)
     expectRule("ts/prefer-nullish-coalescing", dedent`
-        let a: null | string
-        let b: null | string
-        a || b
+        let a: null | string;
+        let b: null | string;
+        a || b;
     `)
     expectRule("ts/prefer-nullish-coalescing", dedent`
-        let a: null | string
-        let b: null | string
-        a ?? b
+        let a: null | string;
+        let b: null | string;
+        a ?? b;
     `, { expected: false })
     expectRule("ts/prefer-nullish-coalescing", dedent`
-        let a: null | string
-        let b: null | string
-        let c: null | string
-        a || (b && c)
+        let a: null | string;
+        let b: null | string;
+        let c: null | string;
+        a || (b && c);
     `, { expected: false })
     expectRule("ts/prefer-nullish-coalescing", dedent`
-        let a: null | string
-        let b: null | string
-        let c: null | string
-        a ?? (b && c)
+        let a: null | string;
+        let b: null | string;
+        let c: null | string;
+        a ?? (b && c);
     `, { expected: false })
     expectRule("ts/prefer-nullish-coalescing", dedent`
-        let a: null | string
-        let b = a || "a string"
+        let a: null | string;
+        let b = a || "a string";
     `)
     expectRule("ts/prefer-nullish-coalescing", dedent`
-        let a: null | string
-        let b = a ?? "a string"
+        let a: null | string;
+        let b = a ?? "a string";
     `, { expected: false })
     expectRule("style-ts/no-extra-parens", dedent`
-        const a = 1
-        const foo = a > 1 ? 0 : (a < 1 ? 1 : 2)
+        const a = 1;
+        const foo = a > 1 ? 0 : (a < 1 ? 1 : 2);
     `, { expected: false })
     expectRule("style-ts/no-extra-parens", dedent`
-        const a = 1
-        const b = 2
+        const a = 1;
+        const b = 2;
         const foo = (a > b) ? a : b
     `, { expected: false })
     // fields without block comment should not be split
