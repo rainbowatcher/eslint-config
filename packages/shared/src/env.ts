9164f468eb85d3ac9487e0627d8e3fa510bdf915
rainbowatcher <rainbow-w@qq.com>
2024-08-25 15:15:44 +0800

    fix(shared): add hasVue and isVue3 function and local-pkg dep

--- packages/shared/src/env.ts
+++ packages/shared/src/env.ts
@@ -1,4 +1,5 @@
 import process from "node:process"
+import { getPackageInfoSync, isPackageExists } from "local-pkg"
 
 export function isInEditor(): boolean {
     if (process.env.CI) return false
@@ -18,4 +19,21 @@ export function isInGitHooksOrLintStaged(): boolean {
         ?? process.env.VSCODE_GIT_COMMAND
         ?? process.env.npm_lifecycle_script?.startsWith("lint-staged")
     )
+}
+
+export function hasVue(): boolean {
+    return isPackageExists("vue")
+}
+
+export function isVue3(): boolean {
+    if (!hasVue()) return false
+    const pkg = getPackageInfoSync("vue")
+    if (
+        pkg
+        && typeof pkg.version === "string"
+        && !Number.isNaN(+pkg.version[0])
+    ) {
+        return +pkg.version[0] === 3
+    }
+    return false
 }
