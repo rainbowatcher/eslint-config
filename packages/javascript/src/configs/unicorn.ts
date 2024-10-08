46cdd308be704d6f2fa2cbc5f2ff78be685666f4
rainbowatcher <rainbow-w@qq.com>
2024-10-08 16:17:42 +0800

    feat(javascript): explicit declaration some rules in plugin unicorn

--- packages/javascript/src/configs/unicorn.ts
+++ packages/javascript/src/configs/unicorn.ts
@@ -12,12 +12,17 @@ export async function unicorn(opts: Options): Promise<EslintFlatConfigItem> {
         name: "rainbowatcher:js:unicorn",
         rules: {
             ...pluginUnicorn.configs.recommended.rules,
+            "unicorn/better-regex": "off",
+            "unicorn/consistent-existence-index-check": "error",
             "unicorn/consistent-function-scoping": "off",
             "unicorn/filename-case": "off",
+            "unicorn/no-array-push-push": "error",
             "unicorn/no-array-reduce": "error",
             "unicorn/no-empty-file": "error",
             "unicorn/no-null": "off",
             "unicorn/prefer-export-from": "off",
+            "unicorn/prefer-global-this": "error",
+            "unicorn/prefer-math-min-max": "error",
             // It doesn't work well. If there are more configuration items, We will try to open it.
             "unicorn/prefer-ternary": "off",
             "unicorn/prevent-abbreviations": "off",
