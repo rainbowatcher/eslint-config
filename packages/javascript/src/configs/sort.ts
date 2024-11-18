d614636c3d699d04af4102b9a2db57bd6c9db555
rainbowatcher <rainbow-w@qq.com>
2024-11-18 17:01:08 +0800

    feat(javascript): add partition by comment option

--- packages/javascript/src/configs/sort.ts
+++ packages/javascript/src/configs/sort.ts
@@ -27,10 +27,10 @@ export function sort(opts: Options): EslintFlatConfigItem {
                     ["get-method", "set-method"],
                     "unknown",
                 ],
-                partitionByComment: false,
+                partitionByComment: ["region", "REGION"],
             }],
             // partition comment same as region comment in vscode
-            "perfectionist/sort-enums": ["error", { partitionByComment: "region", ...commonOptions }],
+            "perfectionist/sort-enums": ["error", { partitionByComment: ["region", "REGION"], ...commonOptions }],
             // TODO: enable it when support astro
             // "perfectionist/sort-astro-attributes": "off",
             "perfectionist/sort-exports": ["error", commonOptions],
@@ -86,7 +86,7 @@ export function sort(opts: Options): EslintFlatConfigItem {
                 destructureOnly: false,
                 groups: [],
                 ignorePattern: [],
-                partitionByComment: [],
+                partitionByComment: ["region", "REGION"],
                 partitionByNewLine: true,
                 styledComponents: true,
             }],
