57f7e7729b71e49e0cec4c91c0f77790b50c0ae4
rainbowatcher <rainbow-w@qq.com>
2024-11-22 12:38:41 +0800

    fix(javascript): remove unused imports

--- packages/javascript/src/configs/unicorn.ts
+++ packages/javascript/src/configs/unicorn.ts
@@ -1,5 +1,5 @@
 import {
-    GLOB_JS, GLOB_JSX, GLOB_MARKDOWN_CODE, GLOB_TS, GLOB_TSX, interopDefault,
+    GLOB_JSX, GLOB_MARKDOWN_CODE, GLOB_TSX, interopDefault,
 } from "@rainbowatcher/eslint-config-shared"
 import { getFiles } from "../files"
 import type { EslintFlatConfigItem, Options } from "@rainbowatcher/eslint-config-shared"
